be6dd59a5a86d3e93706e4f083e9d54510c6ceca
MaksLykh <joke_night_crow@mail.ru>
2024-11-12 20:52:17 -0800

    Login Class

--- pages/Login.ts
+++ pages/Login.ts
@@ -3,6 +3,12 @@ class Login {
     password: string = '#password'
     loginBtn: string = '#login'
     logOutBtn: string = '#submit'
+
+    loginMethod(){
+        cy.get(this.userName).type('test')
+        cy.get(this.password).type('Test1234#')
+        cy.get(this.loginBtn).click()
+    }
 }
 
 export const LoginPage = new Login()
