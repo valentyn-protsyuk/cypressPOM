28d78f26d77f64a4edb4e58990977bbd38b21d01
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-18 13:44:30 -0800

    custom login command done

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -28,7 +28,13 @@
 declare global {
   namespace Cypress {
     interface Chainable {
-      login(email: string, password: string): Chainable<void>
+      login(userName: string, password: string): Chainable<void>;
     }
   }
 }
+
+Cypress.Commands.add("login", (userName:string, password:string)=>{
+    cy.get('#userName').type(userName)
+    cy.get('#password').type(password)
+    cy.contains('button', 'Login').click()
+})
